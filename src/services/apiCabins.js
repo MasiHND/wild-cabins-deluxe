9e894ba383bcaf45b43ed76c3981d0e358eaae7a
MasiHND <pouya.hendy@gmail.com>
2025-03-03 13:54:08 +0330

    added react-query and remote state management

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,10 +1,67 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
+
   if (error) {
     console.log(error);
-    throw new Error("Cabin Could Not Be  Loaded!");
+    throw new Error("Cabin Could Not Be Loaded!");
+  }
+  return data;
+}
+
+export async function createEditCabin(newCabin, id) {
+  const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `${supabaseUrl}/storage/v1/object/public/cabin-images//${imageName}`;
+
+  //Create/Edit
+  let query = supabase.from("cabins");
+
+  //Create (inset row + select)
+  if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
+
+  //Edit
+  if (id) query = query.update({ ...newCabin, image: imagePath }).eq("id", id);
+
+  const { data, error } = await query.select().single();
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabin Could Not Be Created!");
+  }
+
+  //Upload Image
+  if (hasImagePath) return data;
+  
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+
+    console.log(storageError);
+    throw new Error(
+      "Cabin Image Could Not Be Uploaded & Cabin Did Not Created!"
+    );
+  }
+
+  return data;
+}
+
+//Delete
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabin Could Not Be Deleted!");
   }
   return data;
 }
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://udqpqheyoseuglkkdbbo.supabase.co";
+export const supabaseUrl = "https://udqpqheyoseuglkkdbbo.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkcXBxaGV5b3NldWdsa2tkYmJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4MjUxNzUsImV4cCI6MjA1NjQwMTE3NX0.VBHITlRYhqhmtZN1pDsYXh_ms3YPA1juVRCEEzoebl4";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
