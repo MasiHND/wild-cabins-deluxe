074ff87b3b7ffa39f01a73d0bb431acbf4473f7f
MasiHND <pouya.hendy@gmail.com>
2025-03-08 13:57:48 +0330

    added bkooking filtering / API side filtering

--- src/features/bookings/BookingTableOperations.jsx
+++ src/features/bookings/BookingTableOperations.jsx
@@ -1,31 +1,34 @@
 import SortBy from "../../ui/SortBy";
 import Filter from "../../ui/Filter";
 import TableOperations from "../../ui/TableOperations";
+import Row from "../../ui/Row";
 
 function BookingTableOperations() {
   return (
     <TableOperations>
-      <Filter
-        filterField="status"
-        options={[
-          { value: "all", label: "All" },
-          { value: "checked-out", label: "Checked out" },
-          { value: "checked-in", label: "Checked in" },
-          { value: "unconfirmed", label: "Unconfirmed" },
-        ]}
-      />
+      <Row type="vertical">
+        <Filter
+          filterField="status"
+          options={[
+            { value: "all", label: "All" },
+            { value: "checked-out", label: "Checked out" },
+            { value: "checked-in", label: "Checked in" },
+            { value: "unconfirmed", label: "Unconfirmed" },
+          ]}
+        />
 
-      <SortBy
-        options={[
-          { value: "startDate-desc", label: "Sort by date (recent first)" },
-          { value: "startDate-asc", label: "Sort by date (earlier first)" },
-          {
-            value: "totalPrice-desc",
-            label: "Sort by amount (high first)",
-          },
-          { value: "totalPrice-asc", label: "Sort by amount (low first)" },
-        ]}
-      />
+        <SortBy
+          options={[
+            { value: "startDate-desc", label: "Sort by date (recent first)" },
+            { value: "startDate-asc", label: "Sort by date (earlier first)" },
+            {
+              value: "totalPrice-desc",
+              label: "Sort by amount (high first)",
+            },
+            { value: "totalPrice-asc", label: "Sort by amount (low first)" },
+          ]}
+        />
+      </Row>
     </TableOperations>
   );
 }
--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,29 +1,35 @@
 import TableOperations from "../../ui/TableOperations";
 import Filter from "../../ui/Filter";
 import SortBy from "../../ui/SortBy";
+import Row from "../../ui/Row";
 
 function CabinTableOperations() {
   return (
     <TableOperations>
-      <Filter
-        filterField="discount"
-        options={[
-          { value: "all", label: "All" },
-          { value: "no-discount", label: "No Discount" },
-          { value: "with-discount", label: "With Discount" },
-        ]}
-      />
+      <Row type="vertical">
+        <Filter
+          filterField="discount"
+          options={[
+            { value: "all", label: "All" },
+            { value: "no-discount", label: "No Discount" },
+            { value: "with-discount", label: "With Discount" },
+          ]}
+        />
 
-      <SortBy
-        options={[
-          { value: "name-asc", label: "Sort By Name (Ascending)" },
-          { value: "name-desc", label: "Sort By Name (Descending)" },
-          { value: "regularPrice-asc", label: "Sort By Price (Low First)" },
-          { value: "regularPrice-desc", label: "Sort By Price (High First)" },
-          { value: "maxCapacity-asc", label: "Sort By Capacity (Low First)" },
-          { value: "maxCapacity-desc", label: "Sort By Capacity (High First)" },
-        ]}
-      />
+        <SortBy
+          options={[
+            { value: "name-asc", label: "Sort By Name (Ascending)" },
+            { value: "name-desc", label: "Sort By Name (Descending)" },
+            { value: "regularPrice-asc", label: "Sort By Price (Low First)" },
+            { value: "regularPrice-desc", label: "Sort By Price (High First)" },
+            { value: "maxCapacity-asc", label: "Sort By Capacity (Low First)" },
+            {
+              value: "maxCapacity-desc",
+              label: "Sort By Capacity (High First)",
+            },
+          ]}
+        />
+      </Row>
     </TableOperations>
   );
 }
